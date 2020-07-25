bbeb9e86e1c8ecd16aa9fd26c12fc85bd9747634
motezgh <motez.gharbi@hotmail.com>
2020-07-25 15:19:02 +0100

    Dialogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,10 +22,12 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component'
+
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
-import {LeaderService} from './services/leader.service'
+import {LeaderService} from './services/leader.service';
 
 @NgModule({
   declarations: [
@@ -39,7 +44,9 @@ import {LeaderService} from './services/leader.service'
     
     AboutComponent,
     
-    ContactComponent
+    ContactComponent,
+    
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -50,12 +57,16 @@ import {LeaderService} from './services/leader.service'
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule
   ],
   providers: [
     DishService,
     PromotionService,
     LeaderService
+  ],
+  entryComponents: [
+    LoginComponent
   ],
   bootstrap: [AppComponent]
 })
