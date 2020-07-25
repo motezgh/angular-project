f1bb59224bf490d145486db37751eb53874d3987
motezgh <motez.gharbi@hotmail.com>
2020-07-25 01:29:54 +0100

    Single Page Applications Part 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ import { ContactComponent } from './contact/contact.component';
 const routes: Routes = [
   { path: 'home',  component: HomeComponent },
   { path: 'menu',     component: MenuComponent },
+  { path: 'dishdetail/:id', component: DishdetailComponent },
   { path: 'contactus', component: ContactComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
